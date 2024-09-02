1f22d57935e882e4ef3f5e0fbbeb176bdfd78e6e
PillowPowa <fagodfagod@gmail.com>
2024-09-02 13:41:57 +0300

    refactor: separate raw queries from repo

--- server/internal/respository/postgres/image.go
+++ server/internal/respository/postgres/image.go
@@ -32,17 +32,15 @@ func NewImageRepository(db *sqlx.DB) *imageRepository {
 }
 
 func (r *imageRepository) Create(ctx context.Context, image *domain.Image) (*domain.Image, error) {
-	q := `
-  INSERT INTO images (author_id, path, title, description, access_level, expires_at)
-  VALUES ($1, $2, $3, $4, COALESCE(NULLIF($5, '')::access_level, 'public'::access_level), $6) RETURNING *`
-
 	img := new(domain.Image)
 	rowx := r.db.QueryRowxContext(
-		ctx, q,
+		ctx,
+		createImageQuery,
 		image.AuthorID,
 		image.Path,
 		image.Title,
 		image.Description,
+		image.PHash,
 		image.AccessLevel,
 		image.ExpiresAt,
 	)
@@ -54,10 +52,8 @@ func (r *imageRepository) Create(ctx context.Context, image *domain.Image) (*dom
 }
 
 func (r *imageRepository) GetById(ctx context.Context, id int) (*domain.Image, error) {
-	q := `SELECT * FROM images WHERE id = $1`
-
 	img := new(domain.Image)
-	rowx := r.db.QueryRowxContext(ctx, q, id)
+	rowx := r.db.QueryRowxContext(ctx, getByIdImageQuery, id)
 
 	if err := rowx.StructScan(img); err != nil {
 		if err == sql.ErrNoRows {
@@ -69,10 +65,18 @@ func (r *imageRepository) GetById(ctx context.Context, id int) (*domain.Image, e
 	return img, nil
 }
 
-func (r *imageRepository) Delete(ctx context.Context, id int) error {
-	q := `DELETE FROM images WHERE id = $1`
+func (r *imageRepository) Similar(ctx context.Context, id int) ([]domain.Image, error) {
+	_ = `
+  SELECT id FROM 
+    (SELECT id, filephash_1, BIT_COUNT(filephash_2 ^ CONV(SUBSTRING('813ed36913ec8639', 9,  8), 16, 10)) as BC1     FROM phashs WHERE BIT_COUNT(filephash_2 ^ CONV(SUBSTRING('813ed36913ec8639', 9,  8), 16, 10)) <= 3) BCQ1 
+    WHERE BIT_COUNT(filephash_1 ^ CONV(SUBSTRING('813ed36913ec8639', 1,  8), 16, 10)) + BC1
+  `
 
-	if _, err := r.db.ExecContext(ctx, q, id); err != nil {
+	return nil, nil
+}
+
+func (r *imageRepository) Delete(ctx context.Context, id int) error {
+	if _, err := r.db.ExecContext(ctx, deleteImageQuery, id); err != nil {
 		return err
 	}
 
@@ -83,45 +87,7 @@ func (r *imageRepository) GetDetailed(ctx context.Context, id int) (*domain.Deta
 	var detailedImage domain.DetailedImage
 	var tagsJSON []byte
 
-	query := `
-  SELECT
-    i.id,
-    i.author_id,
-    i.path,
-    i.title,
-    i.description,
-    i.access_level,
-    i.expires_at,
-    i.uploaded_at,
-    i.updated_at,
-    u.id AS "author.id",
-    u.username AS "author.username",
-    u.avatar_url AS "author.avatar_url",
-    COALESCE(a.likes_count, 0) AS likes,
-    COALESCE(a.views_count, 0) AS views,
-    TO_JSON(COALESCE(
-      ARRAY_AGG(
-        json_build_object('id', t.id, 'name', t.name)
-      ) FILTER (WHERE t.id IS NOT NULL),
-      '{}'
-    )) AS tags
-    FROM
-        images i
-    JOIN
-      users u ON i.author_id = u.id
-    LEFT JOIN
-      images_to_tags it ON i.id = it.image_id
-    LEFT JOIN
-      tags t ON it.tag_id = t.id
-    LEFT JOIN
-      images_analytics a ON a.image_id = i.id
-    WHERE
-      i.id = $1
-    GROUP BY
-      i.id, u.id, a.likes_count, a.views_count
-  `
-
-	err := r.db.QueryRowxContext(ctx, query, id).Scan(
+	err := r.db.QueryRowxContext(ctx, getDetailedImageQuery, id).Scan(
 		&detailedImage.ID,
 		&detailedImage.AuthorID,
 		&detailedImage.Path,
@@ -157,15 +123,10 @@ func (r *imageRepository) GetDetailed(ctx context.Context, id int) (*domain.Deta
 }
 
 func (r *imageRepository) Update(ctx context.Context, id int, image *domain.Image) (*domain.Image, error) {
-	q := `UPDATE images SET
-    title = COALESCE(NULLIF($1, ''), title),
-    description = COALESCE(NULLIF($2, ''), description),
-    access_level = COALESCE(NULLIF($3, '')::access_level, access_level)::access_level,
-    expires_at = COALESCE($4, expires_at)
-  WHERE id = $5 RETURNING *`
-
 	img := new(domain.Image)
-	rowx := r.db.QueryRowxContext(ctx, q,
+	rowx := r.db.QueryRowxContext(
+		ctx,
+		updateImageQuery,
 		image.Title,
 		image.Description,
 		image.AccessLevel,
@@ -184,25 +145,8 @@ func (r *imageRepository) Update(ctx context.Context, id int, image *domain.Imag
 }
 
 func (r *imageRepository) States(ctx context.Context, imageID int, userID int) (*domain.ImageStates, error) {
-	q := `
-  WITH params AS (
-    SELECT $1::int AS image_id, $2::int AS user_id
-  )
-  SELECT 
-    EXISTS (
-      SELECT 1
-      FROM images_to_views v
-      JOIN params p ON v.image_id = p.image_id AND v.user_id = p.user_id
-    ) AS viewed,
-    EXISTS (
-      SELECT 1
-      FROM images_to_likes l
-      JOIN params p ON l.image_id = p.image_id AND l.user_id = p.user_id
-    ) AS liked;
-  `
-
 	states := new(domain.ImageStates)
-	rowx := r.db.QueryRowxContext(ctx, q, imageID, userID)
+	rowx := r.db.QueryRowxContext(ctx, statesImageQuery, imageID, userID)
 	if err := rowx.StructScan(states); err != nil {
 		return nil, errors.Wrap(err, "imageRepository.States.StructScan")
 	}
@@ -216,14 +160,8 @@ func (r *imageRepository) States(ctx context.Context, imageID int, userID int) (
 }
 
 func (r *imageRepository) HasLike(ctx context.Context, imageID int, userID int) (bool, error) {
-	q := `
-  SELECT EXISTS (
-    SELECT 1 FROM images_to_likes WHERE image_id = $1 AND user_id = $2
-  )
-  `
-
 	var hasLike bool
-	rowx := r.db.QueryRowxContext(ctx, q, imageID, userID)
+	rowx := r.db.QueryRowxContext(ctx, hasLikeImageQuery, imageID, userID)
 	if err := rowx.Scan(&hasLike); err != nil {
 		return false, errors.Wrap(err, "imageRepository.HasLike.Scan")
 	}
---
+++ server/internal/respository/postgres/image_query.go
@@ -0,0 +1,73 @@
+package postgres
+
+const createImageQuery = `
+INSERT INTO images (author_id, path, title, description, p_hash, access_level, expires_at)
+VALUES ($1, $2, $3, $4, $5, COALESCE(NULLIF($6, '')::access_level, 'public'::access_level), $7) RETURNING *
+`
+
+const getByIdImageQuery = `SELECT * FROM images WHERE id = $1`
+
+const deleteImageQuery = `DELETE FROM images WHERE id = $1`
+
+const getDetailedImageQuery = `
+SELECT
+  i.id,
+  i.author_id,
+  i.path,
+  i.title,
+  i.description,
+  i.access_level,
+  i.expires_at,
+  i.uploaded_at,
+  i.updated_at,
+  u.id AS "author.id",
+  u.username AS "author.username",
+  u.avatar_url AS "author.avatar_url",
+  COALESCE(a.likes_count, 0) AS likes,
+  COALESCE(a.views_count, 0) AS views,
+  TO_JSON(COALESCE(
+    ARRAY_AGG(
+      json_build_object('id', t.id, 'name', t.name)
+    ) FILTER (WHERE t.id IS NOT NULL),
+    '{}'
+  )) AS tags
+FROM
+  images i
+JOIN
+  users u ON i.author_id = u.id
+LEFT JOIN
+  images_to_tags it ON i.id = it.image_id
+LEFT JOIN
+  tags t ON it.tag_id = t.id
+LEFT JOIN
+  images_analytics a ON a.image_id = i.id
+WHERE
+  i.id = $1
+GROUP BY
+  i.id, u.id, a.likes_count, a.views_count
+`
+
+const updateImageQuery = `
+UPDATE images SET
+  title = COALESCE(NULLIF($1, ''), title),
+  description = COALESCE(NULLIF($2, ''), description),
+  access_level = COALESCE(NULLIF($3, '')::access_level, access_level)::access_level,
+  expires_at = COALESCE($4, expires_at)
+WHERE id = $5 RETURNING *`
+
+const statesImageQuery = `
+WITH params AS (SELECT $1::int AS image_id, $2::int AS user_id)
+SELECT 
+  EXISTS (
+    SELECT 1
+    FROM images_to_views v
+    JOIN params p ON v.image_id = p.image_id AND v.user_id = p.user_id
+  ) AS viewed,
+  EXISTS (
+    SELECT 1
+    FROM images_to_likes l
+    JOIN params p ON l.image_id = p.image_id AND l.user_id = p.user_id
+  ) AS liked;
+`
+
+const hasLikeImageQuery = `SELECT EXISTS (SELECT 1 FROM images_to_likes WHERE image_id = $1 AND user_id = $2)`
