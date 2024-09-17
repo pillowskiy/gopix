2bf9eb78c23eafbeb684507d6a339db0e91ed1bd
PillowPowa <fagodfagod@gmail.com>
2024-09-17 19:05:06 +0300

    feat(repository*): transactional

--- server/internal/respository/postgres/postgres.go
+++ server/internal/respository/postgres/postgres.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
+	repository "github.com/pillowskiy/gopix/internal/respository"
+	"github.com/pkg/errors"
 )
 
 type InTxQueryCall func(tx *sqlx.Tx) (*sqlx.Rows, error)
@@ -67,4 +69,53 @@ func scanToStructSliceOf[T any](rows *sqlx.Rows) ([]T, error) {
 	}
 
 	return dest, nil
+}
+
+type Ext interface {
+	sqlx.ExecerContext
+	sqlx.QueryerContext
+}
+
+type txKey struct{}
+type PostgresRepository struct {
+	db *sqlx.DB
+}
+
+func (r *PostgresRepository) ext(ctx context.Context) Ext {
+	pgExt, ok := ctx.Value(txKey{}).(Ext)
+	if !ok {
+		return r.db
+	}
+
+	return pgExt
+}
+
+func (r *PostgresRepository) DoInTransaction(
+	ctx context.Context, call repository.InTransactionalCall,
+) (err error) {
+	tx, err := r.db.Beginx()
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		if p := recover(); p != nil {
+			_ = tx.Rollback()
+			panic(p)
+		} else if err != nil {
+			xerr := tx.Rollback()
+			if xerr != nil {
+				err = errors.Wrap(err, xerr.Error())
+			}
+
+			fmt.Printf("Catched: %+v", err)
+		} else {
+			err = tx.Commit()
+		}
+	}()
+
+	ctx = context.WithValue(ctx, txKey{}, tx)
+	err = call(ctx)
+
+	return
 }
--- server/internal/respository/repository.go
+++ server/internal/respository/repository.go
@@ -1,8 +1,17 @@
 package repository
 
-import "errors"
+import (
+	"context"
+	"errors"
+)
 
 var (
 	ErrNotFound       = errors.New("not found")
 	ErrIncorrectInput = errors.New("incorrect input")
 )
+
+type InTransactionalCall func(ctx context.Context) error
+
+type Transactional interface {
+	DoInTransaction(context.Context, InTransactionalCall) error
+}
