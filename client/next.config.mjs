29ca8e1a9e5bd6dd8d0a158b14d21f1dd0853b65
PillowPowa <fagodfagod@gmail.com>
2024-09-18 18:27:51 +0300

    build(client): next-app with biome & scss

---
+++ client/next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	standalone: true
+};
+
+export default nextConfig;
---
+++ client/src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from 'next';
+import '@/styles/globals.scss';
+
+export const metadata: Metadata = {
+	title: 'Gopix'
+};
+
+export default function RootLayout({
+	children
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang='en'>
+			<body>{children}</body>
+		</html>
+	);
+}
