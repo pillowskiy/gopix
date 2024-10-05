19f73cbbdde67944ff03c0445164adf7eb3be8a6
PillowPowa <fagodfagod@gmail.com>
2024-10-05 17:25:57 +0300

    refactor: transition to preact, reduce general bundle size

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	standalone: true
+	reactStrictMode: true,
+	standalone: true,
+	bundlePagesRouterDependencies: true,
+	optimizePackageImports: ['@headlessui/react'],
+	swcMinify: true,
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			Object.assign(config.resolve.alias, {
+				'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+				react: 'preact/compat',
+				'react-dom/test-utils': 'preact/test-utils',
+				'react-dom': 'preact/compat'
+			});
+		}
+		return config;
+	}
 };
 
 export default nextConfig;
