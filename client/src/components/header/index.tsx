9783bbd05ac135963a51fbc834c23a87af2d452d
PillowPowa <fagodfagod@gmail.com>
2024-10-09 19:36:33 +0300

    feat: site header

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,10 +1,33 @@
+import { ArrowUpTrayIcon } from '@heroicons/react/16/solid';
+import { AttentionButton, Button } from '../ui/button';
+import UserDropdown from '../user/user-dropdown';
 import styles from './header.module.scss';
+import { BellIcon } from '@heroicons/react/24/outline';
+import { CursorArrowRaysIcon } from '@heroicons/react/20/solid';
 
 export default function Header() {
 	return (
 		<header className={styles.header}>
 			<div className={styles.headerContent}>
-				<h1 className={styles.headerContentTitle}>Gopix</h1>
+				<div className={styles.headerSection}>
+					<span className={styles.headerLogo}>GoPix</span>
+					<AttentionButton style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+						Upload <ArrowUpTrayIcon style={{ height: '16px', width: '16px' }} />
+					</AttentionButton>
+				</div>
+
+				<div className={styles.headerSection}>
+					<div className={styles.headerActions}>
+						<Button size='icon' variant='ghost'>
+							<CursorArrowRaysIcon />
+						</Button>
+						<Button size='icon' variant='ghost'>
+							<BellIcon />
+						</Button>
+					</div>
+
+					<UserDropdown />
+				</div>
 			</div>
 		</header>
 	);
--- client/src/components/ui/button/button.tsx
+++ client/src/components/ui/button/button.tsx
@@ -9,21 +9,35 @@ import cc from 'classcat';
 import styles from './button.module.scss';
 
 const buttonVariantsStyles = {
-	accent: styles.btnAccent,
-	ghost: styles.btnGhost,
-	secondary: styles.btnSecondary
+	accent: styles.btnVariantAccent,
+	ghost: styles.btnVariantGhost,
+	secondary: styles.btnVariantSecondary
+} as const;
+
+const buttonSizesStyles = {
+	micro: styles.btnSizeMicro,
+	small: styles.btnSizeSmall,
+	medium: styles.btnSizeMedium,
+	large: styles.btnSizeLarge,
+	icon: styles.btnSizeIcon
 } as const;
 
 export interface ButtonProps extends HeadlessButtonProps {
 	variant?: keyof typeof buttonVariantsStyles;
+	size?: keyof typeof buttonSizesStyles;
 }
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-	({ children, variant = 'accent', className, ...props }, ref) => {
+	({ children, variant = 'accent', size = 'medium', className, ...props }, ref) => {
 		return (
 			<HeadlessButton
 				ref={ref}
-				className={cc([styles.btn, buttonVariantsStyles[variant], className])}
+				className={cc([
+					styles.btn,
+					buttonVariantsStyles[variant],
+					buttonSizesStyles[size],
+					className
+				])}
 				{...props}
 			>
 				{children}
