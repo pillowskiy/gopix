e383922587e332a99a082b5021294b9a939a72aa
PillowPowa <fagodfagod@gmail.com>
2025-01-11 18:47:55 +0200

    feat(image): mime to extension function mapper

--- server/pkg/image/image.go
+++ server/pkg/image/image.go
@@ -1,6 +1,7 @@
 package image
 
 import (
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
@@ -12,6 +13,26 @@ import (
 	nanoid "github.com/matoous/go-nanoid/v2"
 	"github.com/pillowskiy/imagesize"
 )
+
+var imagesMimeTypeExt = map[string]string{
+	"image/jpeg":       "jpg",
+	"image/png":        "png",
+	"image/gif":        "gif",
+	"image/webp":       "webp",
+	"image/avif":       "avif",
+	"image/svg+xml":    "svg",
+	"image/x-icon":     "ico",
+	"image/bmp":        "bmp",
+	"image/tiff":       "tiff",
+	"video/mp4":        "mp4",
+	"video/webm":       "webm",
+	"video/ogg":        "ogv",
+	"video/x-msvideo":  "avi",
+	"video/quicktime":  "mov",
+	"video/x-matroska": "mkv",
+	"video/x-flv":      "flv",
+	"video/x-ms-wmv":   "wmv",
+}
 
 type ImageInfo struct {
 	Width  int
@@ -31,6 +52,14 @@ func GenerateUniqueFilename(ext string) string {
 	}
 
 	return fmt.Sprintf("%s.%s", str, ext)
+}
+
+func GetExtByMime(mime string) (string, error) {
+	ext, ok := imagesMimeTypeExt[mime]
+	if !ok {
+		return "", errors.New("unsupported mime provided")
+	}
+	return ext, nil
 }
 
 func DetectMimeFileType(reader io.ReadSeeker) (mime string, err error) {
